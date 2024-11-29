925462f3c736e37826295b8edb87b2fcf616a20b
Gerrnperl <gerrnperl@outlook.com>
2024-11-29 21:04:46 +0800

    feat: update sbi_rt to rustsbi and update wrapper

--- src/sbi.rs
+++ src/sbi.rs
@@ -1,6 +1,6 @@
 use core::ops::Add;
 
-use sbi_rt::{Physical, SbiRet};
+use rustsbi::{Console, Physical, Reset, SbiRet};
 
 pub enum ConsolePutError {
     AddrInvalid,
@@ -9,7 +9,7 @@ pub enum ConsolePutError {
 
 /// 输出字符到控制台
 pub fn sbi_console_putchar(ch: usize) -> Result<(), ConsolePutError> {
-    let sbi_ret = sbi_rt::console_write_byte(ch as u8);
+    let sbi_ret = rustsbi::Forward {}.write_byte(ch as u8);
     if sbi_ret.is_ok() {
         Ok(())
     } else {
@@ -25,8 +25,8 @@ pub fn sbi_console_put(s: &str) -> Result<(), ConsolePutError> {
     }
     let phys_addr_lo = s.as_ptr() as usize;
     if let Some(phys_addr_hi) = phys_addr_lo.checked_add(num_bytes - 1) {
-        let phys_addr_hi = phys_addr_hi >> 32;
-        let sbi_ret = sbi_rt::console_write(Physical::new(num_bytes, phys_addr_lo, phys_addr_hi));
+        let sbi_ret =
+            rustsbi::Forward {}.write(Physical::new(num_bytes, phys_addr_lo, phys_addr_hi));
         if sbi_ret.is_ok() {
             Ok(())
         } else {
@@ -37,11 +37,43 @@ pub fn sbi_console_put(s: &str) -> Result<(), ConsolePutError> {
     }
 }
 
+/// SRST 系统重置类型
+///
+/// https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-sys-reset.adoc
+enum ResetType {
+    Shutdown = 0x00000000,
+    ColdReboot = 0x00000001,
+    WarmReboot = 0x00000002,
+}
+
+/// SRST 系统重置原因
+///
+/// https://github.com/riscv-non-isa/riscv-sbi-doc/blob/master/src/ext-sys-reset.adoc
+enum ResetReason {
+    NoReason = 0x00000000,
+    SystemFailure = 0x00000001,
+}
+
+impl From<ResetType> for u32 {
+    fn from(rt: ResetType) -> u32 {
+        rt as u32
+    }
+}
+
+impl From<ResetReason> for u32 {
+    fn from(rr: ResetReason) -> u32 {
+        rr as u32
+    }
+}
+
 pub fn sbi_shutdown(failure: bool) -> ! {
     if failure {
-        sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::SystemFailure);
+        rustsbi::Forward {}.system_reset(
+            ResetType::Shutdown.into(),
+            ResetReason::SystemFailure.into(),
+        );
     } else {
-        sbi_rt::system_reset(sbi_rt::Shutdown, sbi_rt::NoReason);
+        rustsbi::Forward {}.system_reset(ResetType::Shutdown.into(), ResetReason::NoReason.into());
     }
     unreachable!("sbi_shutdown");
 }
