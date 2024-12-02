82e855edae9cc4dad27ebd20f3a0e1363e792034
Gerrnperl <gerrnperl@outlook.com>
2024-12-02 15:02:48 +0800

    feat(build): linker script for debug profile

--- user-build/src/lib.rs
+++ user-build/src/lib.rs
@@ -1,23 +1,36 @@
 use core::include_str;
-pub fn setup_linker() {
+pub fn setup_linker(profile: &str) {
     let linker = format!(
         r#"/**************************************************************************
 * This is a linker script is generated automatically by the build script. *
 * DO NOT MODIFY IT MANUALLY.                                              *
 ***************************************************************************/
 {}"#,
-        include_str!("linker.ld")
+        if profile == "release" {
+            include_str!("linker.ld")
+        } else {
+            include_str!("linker.debug.ld")
+        }
     );
     // copy the linker script to the src directory
     let out_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
-    let linker_path = std::path::Path::new(&out_dir).join("linker.ld");
+    let linker_path = std::path::Path::new(&out_dir).join(if profile == "release" {
+        "linker.ld"
+    } else {
+        "linker.debug.ld"
+    });
     std::fs::write(&linker_path, linker).unwrap();
 }
 
 pub fn setup() {
-    setup_linker();
-    // target
-    println!("cargo:rerun-if-changed=user/linker.ld");
-    println!("cargo:rustc-link-arg=-Tuser/linker.ld");
+    let profile = std::env::var("PROFILE").unwrap_or_else(|_| "debug".to_string());
+    setup_linker(&profile);
+    if profile == "release" {
+        println!("cargo:rergun-if-changed=user/linker.ld");
+        println!("cargo:rustc-link-arg=-Tuser/linker.ld");
+    } else {
+        println!("cargo:rerun-if-changed=user/linker.debug.ld");
+        println!("cargo:rustc-link-arg=-Tuser/linker.debug.ld");
+    }
     println!("cargo:rustc-force-frame-pointers=yes");
 }
