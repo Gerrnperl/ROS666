3a032fb6c4fc344be5a8e251313b2960a58a99df
Gerrnperl <Gerrnperl@users.noreply.github.com>
2024-12-02 07:40:33 +0000

    deploy: b5d09c0d58217e37615c061d555ac446d7b6d21f

--- crates.js
+++ crates.js
@@ -1,2 +1,2 @@
-window.ALL_CRATES = ["bye","hello","lib","rustsbi","sbi_rt","sbi_spec","startup_test"];
-//{"start":21,"fragment_lengths":[5,8,6,10,9,11,15]}
+window.ALL_CRATES = ["bye","hello","lib","ros666","rustsbi","sbi_rt","sbi_spec"];
+//{"start":21,"fragment_lengths":[5,8,6,9,10,9,11]}
