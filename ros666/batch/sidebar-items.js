7e0b94b311f479b7a2afa207b94f92c4dc5d764e
Gerrnperl <Gerrnperl@users.noreply.github.com>
2024-12-04 05:03:55 +0000

    deploy: cade443fec1b3070384fc8903803fa55f8600053

--- ros666/batch/sidebar-items.js
+++ ros666/batch/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["APP_BASE_ADDRESS","KERNEL_STACK_SIZE","MAX_APP_NUM","USER_STACK_SIZE"],"fn":["run_next_app"],"static":["KERNEL_STACK","USER_STACK"],"struct":["AppManager","KernelStack","UserStack"],"trait":["Stack"]};
+window.SIDEBAR_ITEMS = {"constant":["KERNEL_STACK_SIZE","MAX_APP_NUM","USER_BASE_ADDRESS","USER_SPACE_SIZE","USER_STACK_SIZE"],"fn":["run_next_app"],"static":["KERNEL_STACK","USER_STACK"],"struct":["AppManager","KernelStack","UserStack"],"trait":["Stack"]};
