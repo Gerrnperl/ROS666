cc9187d5a1d0139a24debcfc2954a67cb2bd61d2
Gerrnperl <gerrnperl@outlook.com>
2024-12-04 14:10:59 +0800

    refactor: optimize register save/load with macros in trap.asm

--- src/trap/trap.asm
+++ src/trap/trap.asm
@@ -1,3 +1,11 @@
+.altmacro
+.macro STORE regi, offset, base
+    sd x\regi, \offset(\base)
+.endm
+.macro LOAD regi, offset, base
+    ld x\regi, \offset(\base)
+.endm
+
 .align 2
 .global __save_trap
 __save_trap:
@@ -9,33 +17,12 @@ __save_trap:
     # save registers
     sd x1, 1*8(sp)
     sd x3, 3*8(sp)
-    sd x5, 5*8(sp)
-    sd x6, 6*8(sp)
-    sd x7, 7*8(sp)
-    sd x8, 8*8(sp)
-    sd x9, 9*8(sp)
-    sd x10, 10*8(sp)
-    sd x11, 11*8(sp)
-    sd x12, 12*8(sp)
-    sd x13, 13*8(sp)
-    sd x14, 14*8(sp)
-    sd x15, 15*8(sp)
-    sd x16, 16*8(sp)
-    sd x17, 17*8(sp)
-    sd x18, 18*8(sp)
-    sd x19, 19*8(sp)
-    sd x20, 20*8(sp)
-    sd x21, 21*8(sp)
-    sd x22, 22*8(sp)
-    sd x23, 23*8(sp)
-    sd x24, 24*8(sp)
-    sd x25, 25*8(sp)
-    sd x26, 26*8(sp)
-    sd x27, 27*8(sp)
-    sd x28, 28*8(sp)
-    sd x29, 29*8(sp)
-    sd x30, 30*8(sp)
-    sd x31, 31*8(sp)
+
+    .set rept_i, 5
+    .rept 31-5+1
+        STORE %rept_i, %rept_i*8, sp
+        .set rept_i, rept_i+1
+    .endr
 
     csrr t0, sstatus
     csrr t1, sepc
@@ -47,6 +34,7 @@ __save_trap:
     # call trap handler
     mv a0, sp
     call trap_handler
+
 
 .global __restore_trap
 __restore_trap:
@@ -61,33 +49,11 @@ __restore_trap:
     
     ld x1, 1*8(sp)
     ld x3, 3*8(sp)
-    ld x5, 5*8(sp)
-    ld x6, 6*8(sp)
-    ld x7, 7*8(sp)
-    ld x8, 8*8(sp)
-    ld x9, 9*8(sp)
-    ld x10, 10*8(sp)
-    ld x11, 11*8(sp)
-    ld x12, 12*8(sp)
-    ld x13, 13*8(sp)
-    ld x14, 14*8(sp)
-    ld x15, 15*8(sp)
-    ld x16, 16*8(sp)
-    ld x17, 17*8(sp)
-    ld x18, 18*8(sp)
-    ld x19, 19*8(sp)
-    ld x20, 20*8(sp)
-    ld x21, 21*8(sp)
-    ld x22, 22*8(sp)
-    ld x23, 23*8(sp)
-    ld x24, 24*8(sp)
-    ld x25, 25*8(sp)
-    ld x26, 26*8(sp)
-    ld x27, 27*8(sp)
-    ld x28, 28*8(sp)
-    ld x29, 29*8(sp)
-    ld x30, 30*8(sp)
-    ld x31, 31*8(sp)
+    .set rept_i, 5
+    .rept 31-5+1
+        LOAD %rept_i, %rept_i*8, sp
+        .set rept_i, rept_i+1
+    .endr
 
     # free space for trap frame
     addi sp, sp, 34*8
