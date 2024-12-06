5e1f8a30148f9e8f08129a28fae6016d5247b429
Gerrnperl <gerrnperl@outlook.com>
2024-12-06 11:29:39 +0800

    refactor: simplify app loading

--- src/app_loader.rs
+++ src/app_loader.rs
@@ -60,145 +60,74 @@ impl KernelStack {
     }
 }
 
-lazy_static! {
-    pub static ref APP_LOADER: SyncRefCell<AppLoader> = {
-        SyncRefCell {
-            ref_cell: RefCell::new({
-                let app_count = extern_global!(__app_count) as *const usize;
-                let app_count = unsafe { app_count.read_volatile() };
-                let app_table = extern_global!(__app_table) as *const usize;
-                let app_name_table = extern_global!(__app_name_table) as *const usize;
-                let app_loader = AppLoader::new(app_count, app_table, app_name_table);
-                AppLoader::load_apps(&app_loader);
-                app_loader
-            }),
-        }
+pub fn get_add_count() -> usize {
+    let app_count = extern_global!(__app_count) as *const usize;
+    let app_count = unsafe { app_count.read_volatile() };
+    app_count
+}
+
+pub struct AppData {
+    pub app_id: usize,
+    pub app_name: &'static str,
+    pub data: &'static [u8],
+}
+
+pub fn load_app_data(app_id: usize) -> AppData {
+    let app_count = get_add_count();
+    assert!(app_id < app_count);
+    let app_table = extern_global!(__app_table) as *const usize;
+    let app_name_table = extern_global!(__app_name_table) as *const usize;
+    let app_start = unsafe {
+        core::slice::from_raw_parts(app_table.add(app_id), 1)
+            .get(0)
+            .copied()
+            .unwrap()
     };
-}
-
-pub struct AppLoader {
-    /// 应用程序总数
-    pub app_count: usize,
-    /// 应用程序入口地址表
-    pub app_table: [usize; MAX_APP_NUM],
-    pub app_name_table: [&'static str; MAX_APP_NUM],
-    /// 最后一个应用程序的结束地址
-    pub apps_end: usize,
-}
-
-impl AppLoader {
-    /// 从应用程序表构造一个 AppManager
-    ///
-    /// ## 参数
-    /// - `app_count`：应用程序总数
-    /// - `app_table_ptr`：应用程序入口地址表（的指针）
-    ///
-    /// ## 应用程序表结构
-    /// 应用程序表是一个数组，每个元素是一个应用程序的入口地址（64 位整数）。
-    /// ```asm
-    /// .global __app_table
-    /// __app_table:
-    ///    .quad __app_0_start
-    ///    .quad __app_1_start
-    ///    ; ...
-    ///    .quad __app_{n-1}_start
-    ///    .quad __app_{n-1}_end
-    /// ```
-    ///
-    pub fn new(
-        app_count: usize,
-        app_table_ptr: *const usize,
-        app_name_table_ptr: *const usize,
-    ) -> Self {
-        let app_table_data: &[usize] =
-            unsafe { core::slice::from_raw_parts(app_table_ptr, app_count) }
-                .try_into()
-                .unwrap();
-        let mut app_table = [0; MAX_APP_NUM];
-        app_table[0..app_count].copy_from_slice(app_table_data);
-
-        let apps_end = unsafe {
-            core::slice::from_raw_parts(app_table_ptr.add(app_count), 1)
+    let app_end = if app_id + 1 < app_count {
+        unsafe {
+            core::slice::from_raw_parts(app_table.add(app_id + 1), 1)
                 .get(0)
                 .copied()
                 .unwrap()
-        };
-
-        let app_name_table_data: &[usize] =
-            unsafe { core::slice::from_raw_parts(app_name_table_ptr, app_count) }
-                .try_into()
-                .unwrap();
-        let mut app_name_table = [""; MAX_APP_NUM];
-        for i in 0..app_count {
-            app_name_table[i] = Self::get_app_name(app_name_table_data[i] as *const i8);
         }
-
-        Self {
-            app_count,
-            app_table,
-            apps_end,
-            app_name_table,
-        }
+    } else {
+        extern_global!(__apps_end) as usize
+    };
+    let app_name_ptr = unsafe {
+        core::slice::from_raw_parts(app_name_table.add(app_id), 1)
+            .get(0)
+            .copied()
+            .unwrap()
+    };
+    let app_name = get_app_name(app_name_ptr as *const i8);
+    let app_data =
+        unsafe { core::slice::from_raw_parts(app_start as *const u8, app_end - app_start) };
+    AppData {
+        app_id,
+        app_name,
+        data: app_data,
     }
+}
 
-    pub fn get_app_base_addr(app_id: usize) -> usize {
-        USER_BASE_ADDRESS + app_id * USER_SPACE_SIZE
-    }
+pub fn get_app_name(app_name_ptr: *const i8) -> &'static str {
+    let app_name = unsafe { core::ffi::CStr::from_ptr(app_name_ptr) };
+    let app_name = app_name.to_str().unwrap();
+    app_name
+}
 
-    pub fn get_app_name(app_name_ptr: *const i8) -> &'static str {
-        let app_name = unsafe { core::ffi::CStr::from_ptr(app_name_ptr) };
-        let app_name = app_name.to_str().unwrap();
-        app_name
-    }
-
-    pub fn print_apps_info(&self) {
-        for i in 0..self.app_count {
-            self.print_app_info(i);
-        }
-    }
-
-    pub fn print_app_info(&self, app_id: usize) {
+impl AppData {
+    pub fn print_info(&self) {
         info!(
-            "App {} - Name: {}, Offset: [{:#x}, {:#x}), Base: {:#x}",
-            app_id,
-            self.app_name_table[app_id],
-            self.app_table[app_id],
-            if app_id + 1 < self.app_count {
-                self.app_table[app_id + 1]
-            } else {
-                self.apps_end
-            },
-            Self::get_app_base_addr(app_id)
+            "App {} - Name: {}, Size: {:#x}",
+            self.app_id,
+            self.app_name,
+            self.data.len(),
         );
-    }
-
-    pub fn load_apps(app_loader: &AppLoader) {
-        for i in 0..app_loader.app_count {
-            Self::load_app(&app_loader, i);
-        }
-        unsafe {
-            asm!("fence.i");
-        }
-    }
-
-    fn load_app(app_loader: &AppLoader, app_id: usize) {
-        let app_start = app_loader.app_table[app_id];
-        let app_end = if app_id + 1 < app_loader.app_count {
-            app_loader.app_table[app_id + 1]
-        } else {
-            app_loader.apps_end
-        };
-        let app_size = app_end - app_start;
-        let app_base = AppLoader::get_app_base_addr(app_id);
-        unsafe { core::slice::from_raw_parts_mut(app_base as *mut u8, 0x20000).fill(0) };
-        let app_src = unsafe { core::slice::from_raw_parts(app_start as *const u8, app_size) };
-        let app_dst = unsafe { core::slice::from_raw_parts_mut(app_base as *mut u8, app_size) };
-        app_dst.copy_from_slice(app_src);
     }
 }
 
 pub fn new_app_ctx(appid: usize) -> usize {
-    let base = AppLoader::get_app_base_addr(appid);
+    let base = todo!();
     unsafe {
         KERNEL_STACK[appid].push_ctx(TrapCtx::init_app_context(base, USER_STACK[appid].top()))
     }
