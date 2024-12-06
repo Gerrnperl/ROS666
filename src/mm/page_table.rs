2e4d527fa1588fdeb7d5029a2fbf6ebba0d2c7f6
Gerrnperl <gerrnperl@outlook.com>
2024-12-06 11:22:00 +0800

    feat: page table and mapping functions

--- src/mm/page_table.rs
+++ src/mm/page_table.rs
@@ -1,4 +1,78 @@
-use super::address::{PHYSICAL_PAGE_NUMBER_WIDTH_SV39, PhysicalPageNumber};
+use alloc::vec::Vec;
+
+use super::{
+    address::{PHYSICAL_PAGE_NUMBER_WIDTH_SV39, PhysicalPageNumber, VirtualPageNumber},
+    frame_allocator::{FrameTracker, StackFrameAllocator},
+};
+
+pub struct PageTable {
+    pub root: PhysicalPageNumber,
+    pub frames: Vec<FrameTracker>,
+}
+
+impl PageTable {
+    pub fn new() -> Self {
+        let root = StackFrameAllocator::alloc_frame().unwrap();
+        Self {
+            root: root.frame,
+            frames: Vec::new(),
+        }
+    }
+
+    pub fn map(&mut self, vpn: VirtualPageNumber, ppn: PhysicalPageNumber, flags: PTEFlags) {
+        let entry = self.find_pte(vpn, true).unwrap();
+        if (*entry).valid() {
+            panic!("map: {:?} has been mapped", vpn);
+        }
+        *entry = PageTableEntry::new(ppn, flags);
+    }
+
+    pub fn unmap(&mut self, vpn: VirtualPageNumber) {
+        let entry = self.find_pte(vpn, false).unwrap();
+        if !(*entry).valid() {
+            panic!("unmap: {:?} has not been mapped", vpn);
+        }
+        *entry = PageTableEntry::default();
+    }
+
+    pub fn find_pte(
+        &mut self,
+        vpn: VirtualPageNumber,
+        create: bool,
+    ) -> Option<&mut PageTableEntry> {
+        let indexes = vpn.indexes();
+        let mut page_table = self.root.get_pte_array().get_mut(0).unwrap() as *mut PageTableEntry;
+        for i in 0..3 {
+            let entry = unsafe { &mut *page_table.add(indexes[i]) };
+            if i == 2 {
+                return Some(entry);
+            }
+            if !entry.valid() {
+                if !create {
+                    return None;
+                }
+                let frame = StackFrameAllocator::alloc_frame().unwrap();
+                *entry = PageTableEntry::new(frame.frame, PTEFlags::Valid);
+                self.frames.push(frame);
+            }
+            let next_ppn: PhysicalPageNumber = (&*entry).into();
+            page_table = next_ppn.get_pte_array().get_mut(0).unwrap() as *mut PageTableEntry;
+        }
+        None
+    }
+
+    pub fn translate(&mut self, vpn: VirtualPageNumber) -> Option<PhysicalPageNumber> {
+        self.find_pte(vpn, false).map(|entry| (&*entry).into())
+    }
+
+    pub fn from_satp(satp: usize) -> Self {
+        let root_ppn = PhysicalPageNumber::from(satp);
+        Self {
+            root: root_ppn,
+            frames: Vec::new(),
+        }
+    }
+}
 
 bitflags::bitflags! {
     #[derive(PartialEq)]
