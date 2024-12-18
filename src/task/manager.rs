e9b59f726067ffdf68d0f6a6beb62e691b0b650d
Gerrnperl <gerrnperl@outlook.com>
2024-12-19 00:22:57 +0800

    refactor: simplify TaskManager.
    Some functions will be moved to Processor later

--- src/task/manager.rs
+++ src/task/manager.rs
@@ -1,88 +1,60 @@
 use core::arch::asm;
-use core::cell::RefCell;
+use core::cell::{Ref, RefCell};
 
+use alloc::collections::vec_deque::VecDeque;
+use alloc::sync::Arc;
 use alloc::vec::Vec;
 use lazy_static::lazy_static;
 
 use crate::app_loader::{get_app_count, load_app_data};
 use crate::task::context::TaskCtx;
 use crate::task::switch::__switch;
-use crate::task::task::{TaskControlBlock, TaskStatus};
+use crate::task::task::{ProcessControlBlock, ProcessStatus};
 use crate::trap::context::TrapCtx;
 use crate::utils::safety::SyncRefCell;
 use crate::{info, printkln, sbi::sbi_shutdown};
 
+use super::INIT_PROC;
+use super::processor::Processor;
+
 lazy_static! {
     pub static ref TASK_MANAGER: SyncRefCell<TaskManager> = {
         SyncRefCell {
-            ref_cell: RefCell::new({
-                let app_count = get_app_count();
-                let mut tasks = Vec::with_capacity(app_count);
-                for i in 0..app_count {
-                    tasks.push(TaskControlBlock::new(load_app_data(i)));
-                }
-                TaskManager {
-                    app_count,
-                    current: 0,
-                    tasks,
-                }
-            }),
+            ref_cell: RefCell::new(TaskManager::new()),
         }
     };
 }
 
 pub struct TaskManager {
-    /// 应用程序总数
-    pub app_count: usize,
-    /// 当前运行的应用程序编号
-    pub current: usize,
-    /// 任务控制块数组
-    pub tasks: Vec<TaskControlBlock>,
+    ready_queue: VecDeque<Arc<SyncRefCell<ProcessControlBlock>>>,
 }
 
 impl TaskManager {
-    pub fn start() {
-        info!("Start at task {}", 0);
-        // APP_LOADER.ref_cell.borrow().print_app_info(0);
-        let mut this = TASK_MANAGER.ref_cell.borrow_mut();
-        this.tasks[0].status = crate::task::task::TaskStatus::Running;
-        let next_ptr = &this.tasks[0].ctx as *const TaskCtx;
-        drop(this);
-        let mut null = TaskCtx::default();
-        unsafe {
-            __switch(&mut null, next_ptr);
+    pub fn new() -> Self {
+        TaskManager {
+            ready_queue: VecDeque::new(),
         }
     }
 
-    pub fn switch_to(task_id: usize) {
-        // info!("Switch to task {}", task_id);
-        // APP_LOADER.ref_cell.borrow().print_app_info(task_id);
+    pub fn put(&mut self, task: Arc<SyncRefCell<ProcessControlBlock>>) {
+        self.ready_queue.push_back(task);
+    }
+
+    pub fn get(&mut self) -> Option<Arc<SyncRefCell<ProcessControlBlock>>> {
+        self.ready_queue.pop_front()
+    }
+
+    pub fn put_task(task: Arc<SyncRefCell<ProcessControlBlock>>) {
         let mut this = TASK_MANAGER.ref_cell.borrow_mut();
-        let current = this.current;
-        this.tasks[task_id].status = crate::task::task::TaskStatus::Running;
-        let current_ptr = &mut this.tasks[current].ctx as *mut TaskCtx;
-        let next_ptr = &this.tasks[task_id].ctx as *const TaskCtx;
-        this.current = task_id;
-        drop(this);
-        unsafe {
-            __switch(current_ptr, next_ptr);
-        }
+        this.put(task);
+    }
+
+    pub fn get_task() -> Option<Arc<SyncRefCell<ProcessControlBlock>>> {
+        let mut this = TASK_MANAGER.ref_cell.borrow_mut();
+        this.get()
     }
 
     pub fn cycle_to_next() {
-        let mut this = TASK_MANAGER.ref_cell.borrow_mut();
-        let current = this.current;
-        this.tasks[current].status = crate::task::task::TaskStatus::Ready;
-        drop(this);
-        TaskManager::run_next_task();
-    }
-
-    pub fn replace_to_next() {
-        let mut this = TASK_MANAGER.ref_cell.borrow_mut();
-        let current = this.current;
-        this.tasks[current].status = crate::task::task::TaskStatus::Stopped;
-        drop(this);
-        TaskManager::run_next_task();
     }
 
     pub fn run_next_task() {
@@ -94,30 +66,6 @@ impl TaskManager {
         }
     }
 
-    pub fn get_next_task() -> Option<usize> {
-        let this = TASK_MANAGER.ref_cell.borrow();
-        let current = this.current;
-        // find a task that is ready to run
-        for i in 1..=this.app_count {
-            let task_id = (current + i) % this.app_count;
-            if this.tasks[task_id].status == crate::task::task::TaskStatus::Ready {
-                drop(this);
-                return Some(task_id);
-            }
-        }
-        drop(this);
-        None
-    }
-
-    pub fn current_user_token() -> usize {
-        let mut tm = TASK_MANAGER.ref_cell.borrow_mut();
-        let current = tm.current;
-        tm.tasks[current].get_user_token()
-    }
-
-    pub fn current_trap_cx() -> &'static mut TrapCtx {
-        let tm = TASK_MANAGER.ref_cell.borrow();
-        let current = tm.current;
-        tm.tasks[current].get_trap_cx()
+    pub fn replace_to_next(exit_code: i32) {
     }
 }
