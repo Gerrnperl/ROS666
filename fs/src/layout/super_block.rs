752dac31a2b59b62ebce7c39d657242beacc0e9c
Gerrnperl <gerrnperl@outlook.com>
2024-12-19 17:29:20 +0800

    feat: layout module with SuperBlock structure

---
+++ fs/src/layout/super_block.rs
@@ -0,0 +1,34 @@
+#[repr(C)]
+pub struct SuperBlock {
+    magic: u32,
+    pub total_blocks: u32,
+    pub inode_bitmap: u32,
+    pub inode_blocks: u32,
+    pub data_bitmap: u32,
+    pub data_blocks: u32,
+}
+
+const SUPER_BLOCK_MAGIC: u32 = 0x78887C6A;
+
+impl SuperBlock {
+    pub fn new(
+        total_blocks: u32,
+        inode_bitmap: u32,
+        inode_blocks: u32,
+        data_bitmap: u32,
+        data_blocks: u32,
+    ) -> Self {
+        Self {
+            magic: SUPER_BLOCK_MAGIC,
+            total_blocks,
+            inode_bitmap,
+            inode_blocks,
+            data_bitmap,
+            data_blocks,
+        }
+    }
+
+    pub fn check(&self) -> bool {
+        self.magic == SUPER_BLOCK_MAGIC
+    }
+}
--- fs/src/lib.rs
+++ fs/src/lib.rs
@@ -2,4 +2,5 @@
 pub mod bitmap;
 pub mod block_cache;
 pub mod block_dev;
+pub mod layout;
 extern crate alloc;
