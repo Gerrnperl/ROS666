a0e8483770cf510cbbc0a245f17a10ed3c39fc31
FengSheng0804 <15903641403@163.com>
2024-12-21 11:15:54 +0800

    Annotation of src/trap folders

--- src/trap/handler.rs
+++ src/trap/handler.rs
@@ -1,6 +1,11 @@
+//! 中断处理模块
+
+// 导入核心库中的 asm 和 usize 模块
 use core::{arch::asm, usize};
 
+// 导入公共库中的 Syscall 模块
 use common::syscall::Syscall;
+// 导入 RISC-V 架构相关的中断和寄存器模块
 use riscv::{
     interrupt::{Exception, supervisor::Interrupt},
     register::{
@@ -9,78 +14,106 @@ use riscv::{
     },
 };
 
+// 导入当前 crate 中的模块
 use crate::{
-    extern_global,
-    mm::memory_set::TRAMPOLINE,
-    printk,
-    syscall::syscall,
-    task::{manager::TaskManager, processor::Processor, task::TRAP_CONTEXT},
-    timer::set_next_timeout,
+    extern_global,                                                          // 外部全局变量
+    mm::memory_set::TRAMPOLINE, // 内存设置中的 TRAMPOLINE 常量
+    printk,                     // 打印内核日志的宏
+    syscall::syscall,           // 系统调用处理函数
+    task::{manager::TaskManager, processor::Processor, task::TRAP_CONTEXT}, // 任务管理相关模块
+    timer::set_next_timeout,    // 设置下一个定时器中断的函数
 };
 
+// 导入上级模块中的上下文相关定义
 use super::context::{Riscv64RegAlias, TrapCtx};
 
+/// 定义定时器中断间隔时间（微秒）
 pub const TIMER_INTERVAL_USEC: usize = 10_000;
 
+// 定义一个不进行名称修饰的函数
 #[unsafe(no_mangle)]
+// 用于处理用户态陷入
 pub fn trap_handler() -> ! {
+    // 设置用户态陷阱入口
     set_user_trap_entry();
+    // 读取 scause 和 stval 寄存器的值
     let scause = scause::read().cause();
     let stval = stval::read();
+    // 根据 scause 的值进行匹配处理
     match scause {
+        // 用户态环境调用异常
         scause::Trap::Exception(const { Exception::UserEnvCall as usize }) => {
             let ctx = Processor::current_trap_cx().unwrap();
-            ctx.sepc += 4;
+            ctx.sepc += 4; // 跳过 ecall 指令
             let ret = syscall(Syscall::from(*ctx.a(7)), [*ctx.a(0), *ctx.a(1), *ctx.a(2)]);
-            // ctx 在 syscall 之后会被修改，所以这里需要重新获取
+            // syscall 之后上下文可能会被修改，所以需要重新获取
             let ctx = Processor::current_trap_cx().unwrap();
-            *ctx.a(0) = ret as usize;
+            *ctx.a(0) = ret as usize; // 将系统调用返回值存入 a0 寄存器
         }
+        // 存储错误或存储页错误异常
         scause::Trap::Exception(const { Exception::StoreFault as usize })
         | scause::Trap::Exception(const { Exception::StorePageFault as usize }) => {
-            todo!("Kill the process");
+            todo!("终止进程");
         }
+        // 非法指令异常
         scause::Trap::Exception(const { Exception::IllegalInstruction as usize }) => {
-            todo!("Kill the process");
+            todo!("终止进程");
         }
+        // 监督者定时器中断
         scause::Trap::Interrupt(const { Interrupt::SupervisorTimer as usize }) => {
             // todo!("Timer interrupt");
+            // 设置下一个定时器中断
             set_next_timeout(TIMER_INTERVAL_USEC);
+            // 切换到下一个任务
             TaskManager::cycle_to_next();
         }
+        // 未处理的陷阱
         _ => {
-            panic!("Unhandled trap: {:?}, stval: {:#x}", scause, stval);
+            panic!("未处理的陷阱: {:?}, stval: {:#x}", scause, stval);
         }
     };
+    // 返回到用户态
     trap_return();
 }
 
+/// 设置用户态陷阱入口函数
 fn set_user_trap_entry() {
     unsafe {
+        // 将 stvec 寄存器设置为 TRAMPOLINE 地址，陷阱模式为直接模式
         stvec::write(TRAMPOLINE as usize, TrapMode::Direct);
     }
 }
 
+// 定义一个不进行名称修饰的函数，用于返回到用户态
 #[unsafe(no_mangle)]
+/// 返回到用户态
 pub fn trap_return() -> ! {
+    // 设置用户态陷阱入口
     set_user_trap_entry();
+    // 获取陷阱上下文指针
     let trap_cx_ptr = TRAP_CONTEXT;
+    // 获取当前用户页表的 token
     let user_satp = Processor::current_user_token().unwrap();
+    // 计算 __restore_trap 的虚拟地址
     let restore_va =
         extern_global!(__restore_trap) as usize - extern_global!(__save_trap) as usize + TRAMPOLINE;
     unsafe {
+        // 执行汇编指令，返回到用户态
         asm!(
             "fence.i",
-            "jr {restore_va}",             // jump to new addr of __restore asm function
+            "jr {restore_va}",         // 跳转到 __restore_trap 汇编函数的新地址
             restore_va = in(reg) restore_va,
-            in("a0") trap_cx_ptr,      // a0 = virt addr of Trap Context
-            in("a1") user_satp,        // a1 = phy addr of usr page table
+            in("a0") trap_cx_ptr,      // a0 = 陷阱上下文的虚拟地址
+            in("a1") user_satp,        // a1 = 用户页表的物理地址
             options(noreturn)
         );
     }
 }
 
+// 定义一个不进行名称修饰的函数，用于处理来自内核的陷阱
 #[unsafe(no_mangle)]
+/// 处理来自内核的陷阱
 pub fn trap_from_kernel() -> ! {
+    // 发生来自内核的陷阱时，触发 panic
     panic!("A trap from kernel occurs!");
 }
--- src/trap/init.rs
+++ src/trap/init.rs
@@ -1,3 +1,5 @@
+//! 初始化模块
+
 use core::arch::global_asm;
 
 use riscv::register::stvec::{self, TrapMode};
@@ -6,20 +8,24 @@ use crate::extern_global;
 
 use super::handler::trap_from_kernel;
 
+// 包含汇编代码
 global_asm!(include_str!("trap.asm"));
 
+/// 初始化内核陷阱函数
 pub fn init() {
-    set_kernel_trap_entry();
+    set_kernel_trap_entry(); // 设置内核陷阱入口
 }
 
+/// 启用定时器中断
 pub fn enable_timer_interrupt() {
     unsafe {
-        riscv::register::sie::set_stimer();
+        riscv::register::sie::set_stimer(); // 设置定时器中断使能位
     }
 }
 
+/// 设置内核陷阱入口地址
 fn set_kernel_trap_entry() {
     unsafe {
-        stvec::write(trap_from_kernel as usize, TrapMode::Direct);
+        stvec::write(trap_from_kernel as usize, TrapMode::Direct); // 写入陷阱处理函数地址
     }
 }
