bc7e78a50bdf2a1f46585d48be751a48108c7a5c
Gerrnperl <gerrnperl@outlook.com>
2024-12-24 11:28:01 +0800

    feat: update echo to return an integer value

--- user/src/echo/bin/main.rs
+++ user/src/echo/bin/main.rs
@@ -4,6 +4,7 @@
 use lib::println;
 
 #[unsafe(no_mangle)]
-pub fn main(_argc: usize, argv: &[&str]) {
+pub fn main(_argc: usize, argv: &[&str]) -> i32 {
     println!("{}", argv.join(" "));
+    0
 }
